b7a7b3001ed661ef233b144d1c499f5b65eeb1e4
lai_luv <l.lavaughn319@gmail.com>
2023-10-23 12:40:27 -0400

    final

--- scripts.js
+++ scripts.js
@@ -43,11 +43,11 @@ function alertRandom() {
 
 // Declare a function that prompts the user's for their favorite drink and logs the following phrase to the console. "Your favorite drink is ________."
 
-function favDrinkData() {
- let favDrink = prompt( "Your Favorite Drink is_______?" )
- console.log (`your favorite drink is ${favDrink}.`)
-}
- favDrinkData()
+// function favDrinkData() {
+//  let favDrink = prompt( "Your Favorite Drink is_______?" )
+//  console.log (`your favorite drink is ${favDrink}.`)
+// }
+//  favDrinkData()
 
 
 // Execute the function so that you see "Your favorite drink is [whatever you typed in the prompt]." in the console.
@@ -68,13 +68,13 @@ let user = {
 // Make sure not to use the literal string "Trinity" or the words in the `languages` array.
 
 function Welcome (){
-  console.log(user.fullName.slice(0,8)+ "!  You know " + user.languages +".")
+  alert(user.fullName.slice(0,8)+ "!  You know " + user.languages +".")
 
   
 }
 // Execute the function.
 
-Welcome()
+// Welcome()
 
 // Below, NOT in a function, change the object's properties in these ways:
 // 1. The name should be "Kristal Thomas".
@@ -84,7 +84,7 @@ user.languages[1]='Racket'
 
 
 // Execute the function again, and you should see a different message printed than the first time, with a different name and a different second language listed.
-  Welcome(user)
+  // Welcome()
 
 //Feel free to comment out the function calls once you've confirmed they work.
 
@@ -94,9 +94,11 @@ user.languages[1]='Racket'
 // Declare a function that prompts the user for their birth year and prints to the console the sentence, "Your approximate age is [the value in the age variable]."
 
 function birthday(){
-  birthYear
+  let birthYear = prompt("what is your birth year?");
+  let todaysDate = new Date();
+  let todaysYear = todaysDate.getFullYear();
+  let age = todaysYear - Number(birthYear)
   console.log("your approximate age is  " + age)
-  
 }
 
 // Hint: Don't forget that values come in from a user as strings! Subtracting a string from `todaysYear` won't quite work.
@@ -104,44 +106,42 @@ function birthday(){
 
 // You can use the following code in your function to calculate the user's approximate age:
 
-let birthYear = prompt("what is your birth year?");
-let todaysDate = new Date();
-let todaysYear = todaysDate.getFullYear();
-let age = todaysYear - Number(birthYear);
+
 
 
 // Execute the function. Feel free to comment it out once you've confirmed it works.
 
-birthday()
+// birthday()
 
 /*********************** Exercise 7 ********************* */
 
 // Declare a function that prompts the user for the following things: age, favoriteColor, favoriteYour.
 // Inside the function, create an object to store the user's responses.
 // Inside the function, log the following sentence to the console with the user's responses: "Your age is ____, your favorite color is ______, and your favorite movie is ______".
-function getUserPreferences(){
-  let age = prompt('what is your age?');
-  let favoriteColor = prompt('what is your favorite color?')
-  let favMovie = prompt('Whats your favorite movie ?')
-  let userPreferences ={
-    age:age,
-    favoriteColor:favoriteColor,
-    favoriteMovie:favMovie,
+// function getUserPreferences(){
+//   let age = prompt('what is your age?');
+//   let favoriteColor = prompt('what is your favorite color?')
+//   let favMovie = prompt('Whats your favorite movie ?')
+//   let userPreferences ={
+//     age:age,
+//     favoriteColor:favoriteColor,
+//     favoriteMovie:favMovie,
 
-  };
-  console.log(`your age is ${userPreferences.age}, your favorite corlor is ${userPreferences.favoriteColor} and your favorite movie is ${userPreferences.favoriteMovie}.`)
-}
-// Execute the function
-getUserPreferences()
+//   };
+//   console.log(`your age is ${userPreferences.age}, your favorite corlor is ${userPreferences.favoriteColor} and your favorite movie is ${userPreferences.favoriteMovie}.`)
+// }
+// // Execute the function
+// getUserPreferences()
 /*********************** Exercise 8 ********************* */
 
 // Declare a function that prompts the user for two numbers. Add the two numbers together and log the result to the console.
 function doMath(){
-  let num1= prompt('give a number .')
-  let num2 = prompt('give me another number.')
+  let num1= Number(prompt('give a number .'))
+  let num2 = Number(prompt('give me another number.'))
   return num1 + num2 
 }
 // Execute the function three times.
 let result1 = doMath()
+console.log(result1)
 let result2 = doMath()
 let result3 = doMath()
